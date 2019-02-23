9e24e74483d73edd6cd4f083771261ca533052b6
sining06 <46968513+sining06@users.noreply.github.com>
2019-02-23 21:13:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.menu a').click(function () {
+
+    $('.trigger').toggle();
+    $('.menu').toggleClass('round');
+    $('.close').toggle();
+    $('.drop-left').toggleClass('left');
+    $('.header').toggleClass('blur2');
+});
